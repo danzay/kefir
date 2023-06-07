7981cee05d2cdcd0f230b0e397df8a84c1a0307f
lialiseitbekova <lialiseitbekova@gmail.com>
2023-06-07 21:58:11 +0300

    Add constants.

---
+++ src/constants/constants.ts
@@ -0,0 +1,14 @@
+/** The module keeps constants. */
+
+import { elementSizeType } from "../models";
+
+export const DATE_TEMPLATES = {
+    COMMENTS: "dd.MM.yyyy, HH:mm:ss"
+};
+
+export const  SIZE_VALUES: Record<elementSizeType, number> = {
+    s: 22,
+    m: 30,
+    l: 38
+}
+
