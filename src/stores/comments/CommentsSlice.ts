08a59b58021d43558552701d11c7f31257a47a0d
lialiseitbekova <lialiseitbekova@gmail.com>
2023-06-07 21:51:29 +0300

    Add changing of likes amount in comment section.

--- src/stores/comments/CommentsSlice.ts
+++ src/stores/comments/CommentsSlice.ts
@@ -1,12 +1,12 @@
 /** A storage for comments section. */
 
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { IAuthor, IComment } from "../../pages";
+import { IAuthor, IComment } from "../../models/";
 import { authorsConfig, commentsConfig } from "./CommentsRequests";
 
 interface ICommentsState {
+    commentsStorage: Record<number, IComment>
     authorsData: Record<number, IAuthor>
-    commentsData: IComment[],
     currentPage: number,
     totalPages: number,
     commentsQuantity: number,
@@ -15,8 +15,8 @@ interface ICommentsState {
 }
 
 const initialState: ICommentsState = {
+    commentsStorage: {},
     authorsData: {},
-    commentsData: [],
     currentPage: 0,
     totalPages: 2,
     commentsQuantity: 0,
@@ -28,11 +28,13 @@ export const commentsSlice = createSlice( {
     name: "comments",
     initialState,
     reducers: {
-        addLike: ( state ) => {
+        addLike: ( state, { payload }: PayloadAction<number> ) => {
             state.likesQuantity += 1;
+            state.commentsStorage[ payload ].likes += 1;
         },
-        removeLike: ( state ) => {
+        removeLike: ( state, { payload }: PayloadAction<number> ) => {
             state.likesQuantity -= 1;
+            state.commentsStorage[ payload ].likes -= 1;
         }
     },
     extraReducers: builder => {
@@ -49,14 +51,13 @@ export const commentsSlice = createSlice( {
 
                 if ( payload && state.currentPage !== payload?.pagination?.page ) {
                     const { data, pagination } = payload;
-                    const storage: any = {};
                     const result: IComment[] = [];
 
                     state.totalPages = pagination.total_pages;
                     state.currentPage = pagination.page;
                     state.commentsQuantity += data.length;
                     state.likesQuantity += data.reduce( ( sum: number, comment: IComment ) => {
-                        storage[ comment.id ] = comment;
+                        state.commentsStorage[ comment.id ] = comment;
 
                         return sum + comment.likes;
                     }, 0 );
@@ -65,20 +66,13 @@ export const commentsSlice = createSlice( {
                         if ( comment.parent == null ) {
                             result.push( comment );
                         } else {
-                            const children = storage[ comment.parent ].children || [];
+                            const children = state.commentsStorage[ comment.parent ].children || [];
 
                             children.push( comment );
-                            storage[ comment.parent ].children = children;
+                            state.commentsStorage[ comment.parent ].children = children;
 
                         }
                     } );
-
-                    state.commentsData = [
-                        ...state.commentsData,
-                        ...result.sort( ( a: IComment, b: IComment ) => {
-                            return Date.parse( b.created ) - Date.parse( a.created );
-                        } )
-                    ];
                 }
             }
         );
