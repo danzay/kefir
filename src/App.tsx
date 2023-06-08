af5e4c2fef7bb29b86c937476a971311c8bc8209
lialiseitbekova <lialiseitbekova@gmail.com>
2023-06-08 11:36:12 +0300

    Move themes.ts to theme folder.

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,19 @@ import styled, { ThemeProvider } from "styled-components";
 import { ToastContainer } from "react-toastify";
 
 import "./i18n";
+import { dark } from "./themes/";
 import { Comments } from "./pages/";
 import { mainStore } from "./stores/";
-import backgroundImage from "./assets/background.png";
 import "react-toastify/dist/ReactToastify.css";
-import { dark } from "./themes";
 
 const AppStyle = styled.div`
-  background: no-repeat center black url(${ backgroundImage });
-  background-size: cover;
   height: 100vh;
   font-size: 16px;
   font-family: 'Lato', sans-serif;
   font-weight: 400;
   color: ${ ( props ) => props.theme.colors.text };
+  background: no-repeat center black url(${ ( props ) => props.theme.colors.bgImg });
+  background-size: cover;
 `;
 
 function App() {
---
+++ src/themes/themes.ts
@@ -0,0 +1,15 @@
+import backgroundImage from "../assets/background.png";
+
+export const light: Record<string, any> = {
+    text: '#000000',
+    button: '#ffffff',
+    bg: '#ffffff',
+    bgImg: backgroundImage
+}
+
+export const dark: Record<string, any> = {
+    text: '#ffffff',
+    button: '#313439',
+    bg: '#000000',
+    bgImg: backgroundImage
+}
